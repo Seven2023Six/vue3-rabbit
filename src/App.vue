053a08ab8cc6646be9bce0da74351e6a0fc61103
abc22456 <test@runoob.com>
2025-03-25 14:38:07 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@ import { RouterView } from "vue-router";
 <template>
   <!-- 一级路由出口组件 -->
   <router-view></router-view>
+  <div class="test">Test Scss</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
