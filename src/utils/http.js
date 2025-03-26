212fdc7003d54b1e7d659d3b4b6a5c6f7eb2f1d7
abc22456 <test@runoob.com>
2025-03-26 22:14:47 +0800

    一级导航渲染

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const httpInstance = axios.create({
-    baseURL: "localhost:8421",
+    baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
     timeout: 5000
 })
 
